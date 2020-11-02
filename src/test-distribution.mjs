7ddcd4a303ac1a1312365a483d312c0575469773
Daniel Lopretto <dan@meterfeeder.com>
2020-11-02 15:54:28 -0500

    Calculate CFD from samples.

--- src/test-distribution.mjs
+++ src/test-distribution.mjs
@@ -3,12 +3,21 @@ import { mean, alphaFromHistory, betaFromHistory, sigma, compute } from './distr
 
 const expect = chai.expect;
 
+const round = (x, p = 100000) => Math.round(x * p) / p;
+
 describe('compute gamma', () => {
     it('cdf', () => {
         const cfd = compute(1, 2, 4);
         expect(cfd).to.eql(0.0265);
-
     });
+    it('compute from samples', () => {
+        const sample = [18,16,13,32,22,16,28,37];
+        const alpha = round(alphaFromHistory(sample));
+        const beta = round(betaFromHistory(sample));
+        const cfd = compute(12, alpha, beta);
+        console.log({alpha, beta});
+        expect(cfd).to.eql(0.06702)
+    })
 });
 
 describe('calculate', () => {
@@ -34,20 +43,28 @@ describe('calculate', () => {
             })
             it('return the zeroth element if only one', () => {
                 expect(mean([24])).to.eql(24);
-            })
-            it('returns the alpha for a data sample', () => {
+            });
+            it('calculated', () => {
                 // https://www.wolframalpha.com/input/?i=find+the+sample+mean+of+18%2C16%2C13%2C32%2C22%2C16%2C28%2C37
                 const sample = [18,16,13,32,22,16,28,37];
                 expect(mean(sample)).to.eql(22.75);
             });
         })
     });
+    describe('alpha', () => {
+        it('returns the alpha for the data sample', () => {
+            // https://www.wolframalpha.com/input/?i=+%28mean+%7B18%2C16%2C13%2C32%2C22%2C16%2C28%2C37%7D+%5E+2%29+%2F+%28%28mean+%7B18%5E2%2C16%5E2%2C13%5E2%2C32%5E2%2C22%5E2%2C16%5E2%2C28%5E2%2C37%5E2%7D%29+-+%28mean+%7B18%2C16%2C13%2C32%2C22%2C16%2C28%2C37%7D+%5E+2%29%29
+            const sample = [18,16,13,32,22,16,28,37];
+            const alpha = alphaFromHistory(sample);
+            expect(round(alpha)).to.eql(7.87916);
+        });
+    });
     describe('beta', () => {
         it('returns the beta for a data sample', () => {
             // https://www.wolframalpha.com/input/?i=%28mean+%7B18%5E2%2C16%5E2%2C13%5E2%2C32%5E2%2C22%5E2%2C16%5E2%2C28%5E2%2C37%5E2%7D+-+%28mean+%7B18%2C16%2C13%2C32%2C22%2C16%2C28%2C37%7D+%5E+2%29%29+%2F+%28mean+%7B18%2C16%2C13%2C32%2C22%2C16%2C28%2C37%7D%29
             const sample = [18,16,13,32,22,16,28,37];
             const beta = betaFromHistory(sample);
-            expect(Math.round(beta * 100000) / 100000).to.eql(2.88736);
+            expect(round(beta)).to.eql(2.88736);
         });
     });
 })
